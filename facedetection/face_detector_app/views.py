057737a9122d97460406af77fe1f3b6b2436baf3
abhisek11 <abhisekrock94@gmail.com>
2019-04-27 19:32:04 +0530

    segment 1/3

--- facedetection/face_detector_app/views.py
+++ facedetection/face_detector_app/views.py
@@ -9,4 +9,21 @@ import os
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 
-face_detector = "/home/abhisek/Desktop/haarcascade_frontalface_default.xml"
+face_detector = "/home/abhisek/Desktop/faceai/haarcascade_frontalface_default.xml"
+
+@csrf_exempt
+#default value to be set false 
+def requested_url(request):
+    default = {"safely executed":False} #because no detection yet 
+
+    #betweene GET and POST we go for post request and check for https 
+    if request.method =="POST":
+        if request.FILEs.get("image",None) is not None:
+            image_to_read = read_image(stream=request.FILES["image"])
+
+        else:#url is provided by user 
+            url_provided = request.POST.get()
+
+            if url_provided is None:
+                default["error value"]= "There is no url provided"
+                return JsonResponse(default)
